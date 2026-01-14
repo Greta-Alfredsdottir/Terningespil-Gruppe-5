da7f18c76eed3b470e770e654fe9565d7cebf2cd
Greta <asth0010@elev.techcollege.dk>
2026-01-14 16:20:33 +0100

    header-footer-startside

--- Terningespil/src/App.tsx
+++ Terningespil/src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
 import "./App.css";
+import { useState } from "react";
+import { StartSide } from "./component/startside/startside";
+import { Header } from "./component/startside/header/header";
+import { Footer } from "./component/startside/Footer/footer";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [Start, setStart] = useState(true);
 
-  return <></>;
+  return (
+    <>
+      <Header />
+      <StartSide Start={Start} setStart={setStart}></StartSide>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ Terningespil/src/component/startside/Footer/footer.tsx
@@ -0,0 +1,5 @@
+import style from "./footer.module.scss";
+
+export function Footer() {
+  return <footer className={style.mainFooter}>Dette er vores footer</footer>;
+}
