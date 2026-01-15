31cbe486d90d0eaa7e81267459e6d5d5e893ee09
NightsHigh <soren913@gmail.com>
2026-01-15 12:52:45 +0100

    Replaced background image with image without textand adjusted the css for the site for both mobile and desktop

--- Terningespil/src/component/startside/header/header.tsx
+++ Terningespil/src/component/startside/header/header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <>
       <header className={style.mainHeader}>
-        <h1>DEAD ON DIZE</h1>
+        <h1>DEAD ON DICE</h1>
       </header>
       <div className={style.center}>
         <section className={style.terningOverlay}></section>
